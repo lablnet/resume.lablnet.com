d4305d755fd78355ff60966ad5cbc4a14f62eded
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-05-26 10:32:19 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,43 +1,165 @@
-exports.name = 'Your Name'
-exports.title = "Your title"
-exports.sub_title = "Your sub <b>title</b>"
-exports.description = "Your Bio"
-exports.email = 'Email'
-exports.phone = '92xxxxxxxx'
-exports.profile_url = ""
+exports.name = 'Muhammad Umer Farooq'
+exports.title = "Software Enginner"
+exports.sub_title = "CEO at <a href='https://alphasofthub.com'><b>AlphaSoftHub Pvt Ltd.</b></a>"
+exports.description = "Since the age of 15 yr(s) i have been in computer programming and computer related tasks. <br/> I love to solve problems using computations. I have worked on different projects throughout the years. I have expertise in different computer programming languages and problem solving that includes C++, Python, Dart, PHP etc. Beside all these I am also interested in deep learning, algorithm implementation, kernel, compilers, computer vision etc."
+exports.email = 'mumerfarooqlablnet01@gmail.com'
+exports.phone = null
+exports.profile_url = "https://i.imgur.com/AptkNDm.jpg"
 
 exports.languages = [
   {
-    name: "",
-    subtitle: "",
+    name: "Python",
+    subtitle: "Primary",
+  },
+  {
+    name: "C++",
+    subtitle: "Secondary choice",
+  },
+  {
+    name: "PHP",
+    subtitle: "Primary Backend",
+  },
+  {
+    name: "JavaScript",
+    subtitle: "Primary frontend",
+  },
+  {
+    name: "HTML/CSS",
+    subtitle: "Web",
+  },
+  {
+    name: "Dart/Flutter",
+    subtitle: "App Development",
+  },
+  {
+    name: "NodeJs",
+    subtitle: "Formality",
+  },
+  {
+    name: "Flask",
+    subtitle: "Secoundary Backend",
+  },
+  {
+    name: "VueJs",
+    subtitle: "Frontend",
+  },
+  {
+    name: "Ruby",
+    subtitle: "For Fun",
   }
 ]
 
 exports.tools = [
   {
-    name: "",
-    subtitle: "",
+    name: "GIT",
+    subtitle: "VCS",
+  },
+  {
+    name: "Postman",
+    subtitle: "API inspector",
+  },
+  {
+    name: "Docker",
+    subtitle: "Container",
+  },
+  {
+    name: "VS Code",
+    subtitle: "Default Editor",
+  },
+  {
+    name: "Python",
+    subtitle: "Default Python IDE",
+  },
+  {
+    name: "Clion",
+    subtitle: "Default C++ IDE",
+  },
+  {
+    name: "WebStrom",
+    subtitle: "Default JavaScript IDE",
+  },
+  {
+    name: "IntelliJ IDEA",
+    subtitle: "Default Flutter IDE",
+  },
+  {
+    name: "Windows 10",
+    subtitle: "Primary OS",
+  },
+  {
+    name: "Linux",
+    subtitle: "Secoundary OS",
   }
 ]
 
 exports.projects = [
   {
-    year: "year",
-    name: "name",
-    shortheading: "short heading",
+    year: "2021",
+    name: "COVID-19 Dashboard for Pakistan",
+    shortheading: "Super Fast COVID-19 Dashboard and Parser",
     app: null,
-    code: null,
+    code: "https://github.com/lablnet/covid19-parser",
     desktop: null,
-    web: null,
-    description: null,
+    web: "https://lablnet.github.io/covid19-parser/",
+    description: "As the whole world is facing COVID-19, numbers and its presentation play a vital role in understanding the pandemic accurately. I developed a dashboard that provided COVID-19 information in Pakistan.",
   },
+    {
+      year: "2021",
+      name: "Prayer Time",
+      shortheading: "A Prayer time app",
+      app: "https://play.google.com/store/apps/details?id=prayer_time.lablnet.app",
+      code: "https://github.com/lablnet/prayer_time_app",
+      desktop: null,
+      web: "https://lablnet.github.io/prayer_time_app",
+      description: "An app to find prayers/ramazan timings for you exact location.",
+    },
+    {
+      year: "2020",
+      name: "ResourcesR",
+      shortheading: "An open source study solution",
+      app: "https://play.google.com/store/apps/details?id=resourcesr.web.app",
+      code: "https://github.com/resourcesr",
+      desktop: null,
+      web: "https://resourcesr.web.app",
+      description: "An app that provides a platform to share class learning specially for Riphah Students. Features include class resources sharing, timetable sharing, events, reminders (like assignments deadlines etc), notifications about upcoming deadlines and events.",
+    },
+    {
+      year: "2020",
+      name: "Board",
+      shortheading: "A standalone Board App",
+      app: null,
+      code: "https://github.com/Lablnet/board",
+      desktop: null,
+      web: "http://lablnet.github.io/board",
+      description: "Simple, standalone, flexible and very lightweight board app/PWA in JavaScript.",
+    },
+    {
+      year: "2019",
+      name: "SpendBTC",
+      shortheading: "A BTC exchange solution",
+      app: null,
+      code: null,
+      desktop: null,
+      web: "https://spendbtc.io/",
+      description: "Exchange and spend your Bitcoin and Crypto easily, using SpendBTC.      ",
+    },
+    {
+      year: "2018",
+      name: "Zest Framework",
+      shortheading: "A standalone PHP Framework",
+      app: null,
+      code: "https://github.com/zestframework/Zest_Framework",
+      desktop: null,
+      web: null,
+      description: "Zest is a simple yet powerful PHP MVC framework for rapid application development that is suited for small to medium scale apps and APIs. \nThe purpose of Zest Framework As for PHP, there are many PHP frameworks available nowadays, so the question is raised as to why we/people should use ZestFramework. The main purpose of Zest framework is to provide a very lightweight framework without any external dependencies except core and autoloader.",
+    },
 ]
 
 exports.workExperiences = [
   {
-    year: "year",
-    company: "name",
-    role: "short heading",
+    year: "Nov 2019 - April 2020",
+    company: "Overstep BPO",
+    role: "Web Developer",
     offerLetter: null,
     terminationCertificate: null,
     description: null,
@@ -46,20 +168,30 @@ exports.workExperiences = [
 
 exports.educations = [
   {
-    year: "year",
-    school: "name",
-    program: "",
+    year: "2020-2024",
+    school: "Riphah International University",
+    program: "BSSE CGPA - 4",
+  },
+  {
+    year: "2017-2019",
+    school: "Punjab College for Commence",
+    program: "Intermediate in Computer Science 55%",
+  },
+  {
+    year: "2015-2017",
+    school: "F G boys secondary school",
+    program: "Matriculation with Computer Science 65%",
   },
 ]
 
 exports.hobbies = {
-  title: "",
-  subtitle: "",
-  description: "",
+  title: "What i do when i am free",
+  subtitle: "Random things",
+  description: "Listening Sad music, planting, YouTubing, rarely sometimes electronics, replicating old content specially related to web or app development, a bit mathematics, teaching, and looking to open source projects.  ",
 }
 
 exports.interests = [
   {
-    title: "",
+    title: "Deep Thinking",
   }
 ]
